97e50101ff53599628969ceab7917cebc1ad9f0a
PATEL PRIYA <patelpriya380026@gamil.com>
2026-04-25 19:49:41 +0530

    Neet study tracking system

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: ["https://neet-study-tracking.onrender.com",
+    origin: ["https://neet-study-tracking-204o.onrender.com",
     "http://localhost:5173"],
     credentials: true,
   }), 
