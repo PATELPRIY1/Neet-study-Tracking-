5e01f97b68b1f7c41bec13f5d8493e20920ac447
PATEL PRIYA <patelpriya380026@gamil.com>
2026-04-25 19:17:41 +0530

    Neet study tracking system

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
-const { VITE_API_URL } = process.env;
+
 
 
 const startServer = async () => {
   await connectDB();
-  app.listen(VITE_API_URL || 3000, () => {
-    console.log('Server is running on port ' + (VITE_API_URL || 3000));
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port ' + (process.env.PORT || 3000));
   });
 };
 
