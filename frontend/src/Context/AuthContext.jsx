6b999f7267b4ec2cc20e66ad8d56df729ffffc35
PATEL PRIYA <patelpriya380026@gamil.com>
2026-09-15 20:53:47 +0530

    Neet study tracking system

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
-import axios from "axios";
-const VITE_API_URL = import.meta.env.VITE_API_URL;
+import axios from "../api/axios";
+
 
 export const AuthContext = createContext();
 
