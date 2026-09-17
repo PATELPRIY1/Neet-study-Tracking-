1492505066ecfa86f240e622a4c32d85e8eb3298
PATEL PRIYA <patelpriya380026@gamil.com>
2026-09-17 19:44:30 +0530

    Neet study tracking system

--- frontend/src/pages/WeeklyPlanner.jsx
+++ frontend/src/pages/WeeklyPlanner.jsx
@@ -443,9 +443,8 @@ const WeeklyPlanner = () => {
         <div className="flex justify-between items-center">
           <div className="flex items-center justify-between gap-3 border-b border-white/10 px-6 py-4">
             <div className="flex items-center gap-2">
-              
-            <button
-                className="rounded-lg p-2 text-gray-400 hover:bg-white/10"
+              <button
+                className="rounded-md bg-(--bg-transparent-2-color) backdrop-blur-[14px] backdrop-saturate-150 border border-white/25 shadow-[0_8px_24px_rgba(0,0,0,0.4),inset_0_1px_0_rgba(255,255,255,0.4),inset_0_-2px_6px_rgba(0,0,0,0.2)] focus:outline-2 focus:outline-(--secondary-color) flex items-center gap-2 px-3 py-2 text-sm text-gray-400 hover:bg-white/10"
                 onClick={() =>
                   setSortOrder((prev) => (prev === "asc" ? "desc" : "asc"))
                 }
@@ -455,7 +454,7 @@ const WeeklyPlanner = () => {
                 <span>{sortOrder === "asc" ? "A → Z" : "Z → A"}</span>
               </button>
 
-              <button className="rounded-lg p-2 text-gray-400 hover:bg-white/10">
+              <button className="rounded-md bg-(--bg-transparent-2-color) backdrop-blur-[14px] backdrop-saturate-150 border border-white/25 shadow-[0_8px_24px_rgba(0,0,0,0.4),inset_0_1px_0_rgba(255,255,255,0.4),inset_0_-2px_6px_rgba(0,0,0,0.2)] focus:outline-2 focus:outline-(--secondary-color) flex items-center gap-2 px-3 py-2 text-sm text-gray-400 hover:bg-white/10">
                 <Search size={18} />
                 <input
                   type="text"
